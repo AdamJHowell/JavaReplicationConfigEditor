0e4489cba597065e69d0b7cc55e0e6e1a2be7659
Adam Howell <AdamJHowell@users.noreply.github.com>
2022-02-22 09:02:37 -0700

    Suppressing the Sonar IP address regex warning in quoteIfNeeded().

--- src/com/faircom/replicationconfigeditor/Main.java
+++ src/com/faircom/replicationconfigeditor/Main.java
@@ -370,6 +370,7 @@ public class Main
 			return value;
 		}
 
+		@SuppressWarnings( "squid:S5843" )
 		String ipAddressPatternString = "\\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\b";
 		String digitPatternString = "\\d+";
 		String wordPatternString = "\\w+";
